f4279b377bbebd76a57cdd98c9da95c0959733b6
SpicyBoyd <greenyu0510@gmail.com>
2018-03-11 17:32:31 +0800

    將welcomeActivity改為FrameLayout，並修改背景圖片和新增文字

--- app/src/main/java/com/travel/coastline/WelcomeActivity.java
+++ app/src/main/java/com/travel/coastline/WelcomeActivity.java
@@ -11,40 +11,12 @@ public class WelcomeActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
 
-        //方法1：啟動執行序
-//        new Thread(new Runnable() {
-//            @Override
-//            public void run() {
-//                try{
-//                    Thread.sleep(3000);
-//                    startActivity(new Intent().setClass(WelcomeActivity.this,List.class));
-//                }catch (InterruptedException e){
-//                    e.printStackTrace();
-//                }
-//            }
-//        }).start();
-
-        //方法2：
-        mHandler.sendEmptyMessageDelayed(GOTO_MAIN_ACTIVITY, 2000); //2秒跳轉
-    }
-    private static final int GOTO_MAIN_ACTIVITY = 0;
-    private Handler mHandler = new Handler() {
-        public void handleMessage(android.os.Message msg) {
-
-            switch (msg.what) {
-                case GOTO_MAIN_ACTIVITY:
-                    Intent intent = new Intent();
-                    //將原本Activity的換成MainActivity
-                    intent.setClass(WelcomeActivity.this, MainActivity.class);
-                    startActivity(intent);
-                    finish();
-                    break;
-
-                default:
-                    break;
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(WelcomeActivity.this, MainActivity.class));
+                finish();
             }
-        }
-
-    };
-
+        }, 2000);
+    }
 }
