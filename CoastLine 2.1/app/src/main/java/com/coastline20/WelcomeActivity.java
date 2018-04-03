ae6022dde9f4052aa689cbe8b4b14284479eb947
SpicyBoyd <greenyu0510@gmail.com>
2018-04-03 20:24:50 +0800

    完成 歡迎畫面

---
+++ CoastLine 2.1/app/src/main/java/com/coastline20/WelcomeActivity.java
@@ -0,0 +1,31 @@
+package com.coastline20;
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+public class WelcomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcome);
+
+        Toast.makeText(this, "載入中", Toast.LENGTH_SHORT).show();
+        // 3秒跳轉
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(WelcomeActivity.this, MainActivity.class));
+                finish();
+            }
+        }, 3000);
+    }
+}
