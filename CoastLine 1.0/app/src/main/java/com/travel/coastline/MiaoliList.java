308a0a9ae1de1ee39f2f17918c5627993578bca8
SpicyBoyd <greenyu0510@gmail.com>
2018-04-03 19:24:34 +0800

    更換 資料夾

---
+++ CoastLine 1.0/app/src/main/java/com/travel/coastline/MiaoliList.java
@@ -0,0 +1,13 @@
+package com.travel.coastline;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MiaoliList extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_miaoli_list);
+    }
+}
---
+++ CoastLine 1.0/app/src/main/java/com/travel/coastline/TaichungList.java
@@ -0,0 +1,13 @@
+package com.travel.coastline;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class TaichungList extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_taichung_list);
+    }
+}
