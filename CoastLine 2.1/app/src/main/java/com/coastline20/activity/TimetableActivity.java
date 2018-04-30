246886e8b327753b51d4295a0e9f4ac96a8c82b9
SpicyBoyd <greenyu0510@gmail.com>
2018-05-01 00:19:44 +0800

    修正 webview 上的 JavaScript 沒有啟用的 BUG

--- CoastLine 2.1/app/src/main/java/com/coastline20/activity/TimetableActivity.java
+++ CoastLine 2.1/app/src/main/java/com/coastline20/activity/TimetableActivity.java
@@ -2,6 +2,7 @@ package com.coastline20.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 
@@ -15,6 +16,8 @@ public class TimetableActivity extends AppCompatActivity {
         setContentView(R.layout.activity_webview);
 
         WebView webView = (WebView) findViewById(R.id.web_view);
+        WebSettings webSettings = webView.getSettings();
+        webSettings.setJavaScriptEnabled(true); //与Javascript交互
         webView.setWebViewClient(new WebViewClient());
         webView.loadUrl("http://twtraffic.tra.gov.tw/twrail/mobile/home.aspx");
     }
